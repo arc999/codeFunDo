b536d93f0ca52015ce29a8ad56bebd643369bb83
arc999 <roychoudhury.archit@gmail.com>
2018-10-24 13:28:11 +0530

    database

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
+
+
+
+
+
 from flask import Flask, render_template
+
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'   
+
+
+db = SQLAlchemy(app)
+
+class Location(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	lat = db.Column(db.Float, unique = True)
+	longt = db.Column(db.Float, unique = True)
+	rating = db.Column(db.Integer, default= "3")
+    
+
+def __repr__(self):
+	return f"Location('{self.lat}','{self.longt}','{self.rating}'')"
+
+
+
+
+
 
 @app.route("/")
 def main():
